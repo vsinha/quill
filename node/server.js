1b31fa4d379772671751f75b525cab422776cb8e
Viraj Sinha <virajosinha@gmail.com>
2015-10-31 13:06:03 -0700

    all ready for new project

---
+++ node/server.js
@@ -0,0 +1,53 @@
+var autobahn = require('autobahn');
+
+var connection = new autobahn.Connection({
+   url: 'ws://127.0.0.1:8080/ws',
+   realm: 'realm1'}
+);
+
+connection.onopen = function (session) {
+
+   // subscribe
+
+
+   // example of printing json blobs for updating the canvas
+   // (really just as serverside logging
+   function printJSON (args) {
+      var msg = args[0];
+      console.log("event for 'printJSON' received: " + msg);
+   }
+
+   session.subscribe('com.quill.testsession', printJSON).then(
+      function (sub) {
+         console.log("subscribed to topic 'testsession'");
+      },
+      function (err) {
+         console.log("failed to subscribe: " + err);
+      }
+   );
+
+   // also subscribe to a counter to test the connection
+   function on_counter (args) {
+      var counter = args[0];
+      console.log("on_counter() event received with counter " + counter);
+   }
+   session.subscribe('com.quill.testServerCounter', on_counter).then(
+      function (sub) {
+         console.log("subscribed to topic 'testServerCounter'");
+      },
+      function (err) {
+         console.log("failed to subscribe: " + err);
+      }
+   );
+
+   // publish
+   var counter = 0;
+   setInterval(function () {
+      session.publish('com.quill.testClientCounter', [counter]);
+      console.log("published to 'testcounter' with counter " + counter);
+
+      counter += 1;
+   }, 1000);
+};
+
+connection.open();
