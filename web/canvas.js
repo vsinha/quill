84667fd4fcdf6bca6ef49ae3ddcee75d24a2e6a0
Viraj Sinha <virajosinha@gmail.com>
2015-10-31 21:01:55 -0700

    p2p drawing works!

--- web/canvas.js
+++ web/canvas.js
@@ -51,7 +51,7 @@ window.onload = function() {
     cursor = document.getElementById('cursor');
     texture = document.getElementById('texture');
     plugin = document.getElementById('wtPlugin');
-    //mode.innerHTML = sampling;
+    mode.innerHTML = sampling;
     //texture.innerHTML = "T";
 
     // set up autobahn WAMP connection
@@ -83,11 +83,11 @@ window.onload = function() {
     ////////////
     // BUTTONS
     ////////////
-    /*mode.onclick = function(e) {
+    mode.onclick = function(e) {
       sampling = (sampling === 2) ? 0 : sampling + 1;
       ploma.setSample(sampling);
       mode.innerHTML = sampling; 
-    }*/
+    }
     save.onclick = function(e) {
       window.open(canvas.toDataURL());
     }
@@ -155,46 +155,35 @@ window.onload = function() {
     ///////////////////////////////////
     canvas.onmousedown = function(e) {
         isDrawing = true;
-        if (sampling === 0) return;
+
         ploma.beginStroke(
           e.clientX,
           e.clientY,
           0.9
         );
+        session.publish("com.quill.beginStroke", [e.clientX, e.clientY, 0.9]);
     }
     canvas.onmousemove = function(e) {
         if (!isDrawing) return;
-        //elapsed = Date.now();
-        if (sampling === 0) {
-          //console.log(Date.now() - elapsed);
-          canvas.getContext('2d').putImageData(
-            rid,
-            e.clientX,
-            e.clientY,
-            0,
-            0,
-            2,
-            2
-          );
-          return;
-        }
+
         ploma.extendStroke(
           e.clientX,
           e.clientY,
           0.9
         );
+        session.publish("com.quill.extendStroke", [e.clientX, e.clientY, 0.9]);
     }
     canvas.onmouseup = function(e) {
         isDrawing = false;
-        if (sampling === 0) return;
+
         ploma.endStroke(
           e.clientX,
           e.clientY,
           0.9
         );
         var curstroke = ploma.curStroke();
-        console.log("publishing curstroke: " + JSON.stringify(curstroke));
-        session.publish('com.quill.newStroke',curstroke); 
+
+        session.publish("com.quill.endStroke", [e.clientX, e.clientY, 0.9]);
     }
 
     // timers
@@ -208,59 +197,40 @@ window.onload = function() {
         // SUBSCRIBE to a topic and receive events
         
         // subscribe to new stroke event
-        function newStroke (stroke) {
+        var beginStrokeEventName = "com.quill.beginStroke";
+        session.subscribe(beginStrokeEventName, function (args) {
+            console.log(args);
+            ploma.beginStroke( args[0], args[1], args[2] );
+        }).then(
+            function (sub) { console.log("subscribed to " + beginStrokeEventName); },
+            function (err) { console.log("error subscribing to " + beginStrokeEventName + ": " + err); }
+        );
+        
+        // subscribe to new stroke event
+        var extendStrokeEventName = "com.quill.extendStroke";
+        session.subscribe(extendStrokeEventName, function (args) {
+            console.log(args);
+            ploma.extendStroke( args[0], args[1], args[2] );
+        }).then(
+            function (sub) { console.log("subscribed to " + extendStrokeEventName); },
+            function (err) { console.log("error subscribing to " + extendStrokeEventName + ": " + err); }
+        );
 
-            console.log("New stroke: " + JSON.stringify(stroke));
-            //TODO where in 'args' is this information actually
-            ploma.setStrokes(stroke);
-        }
-        session.subscribe('com.quill.newStroke', newStroke).then(
-            function (sub) {
-                console.log("subscribed to newStroke");
-            },
-            function (err) {
-                console.log("error subscribing to newStroke: " + err);
-            }
+        var endStrokeEventName = "com.quill.endStroke";
+        session.subscribe(endStrokeEventName, function (args) {
+            console.log(args);
+            ploma.endStroke( args[0], args[1], args[2] );
+        }).then(
+            function (sub) { console.log("subscribed to " + endStrokeEventName); },
+            function (err) { console.log("error subscribing to " + endStrokeEventName + ": " + err); }
         );
-        /*
-        function on_counter (args) {
-           var counter = args[0];
-           console.log("on_counter() event received with counter " + counter);
-        }
-        session.subscribe('com.quill.testClientCounter', on_counter).then(
-            function (sub) {
-                console.log('subscribed to topic');
-            },
-            function (err) {
-                console.log('failed to subscribe to topic', err);
-            }
-        );
-        // PUBLISH an event every second
-        //
-        var serverCounter = 0;
-        t1 = setInterval(function () {
-           session.publish('com.quill.testServerCounter', ['Hello from the client: ' + serverCounter]);
-           console.log("published to topic 'com.quill.testServerCounter' value: " + serverCounter);
-           serverCounter += 1;
-        }, 1000);
-        */
     };
+
      // fired when connection was lost (or could not be established)
-     //
     connection.onclose = function (reason, details) {
         console.log("Connection lost: " + reason);
-        /*
-        if (t1) {
-            clearInterval(t1);
-            t1 = null;
-        }
-        if (t2) {
-           clearInterval(t2);
-           t2 = null;
-        }
-        */
     }
+
     // now actually open the connection
-    //
     connection.open();
 }
